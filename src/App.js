b135402d4b630d83cf0dff8ce3beb1345fee1466
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-03-20 17:24:26 +0700

    feat: Add information page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Information</h1>
+      <p>Name: Thuan Nguyen</p>
+      <p>Age: 23</p>
+      <p>Email: thuannguyenhuu11@mail.com</p>
+      <p>Location: Da Nang</p>
     </div>
   );
 }
